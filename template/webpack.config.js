1f77f4b74e614301fa752e669f8c0cee47c84288
MillerRen <ren12345@126.com>
2019-07-08 10:42:30 +0800

    version

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -100,7 +100,8 @@ module.exports = {
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.output.filename = process.env.npm_package_name + '.[name].' + process.env.npm_package_gitHead + '.js'
+  var version = process.env.npm_package_gitHead || process.env.npm_package_version
+  module.exports.output.filename = process.env.npm_package_name + '.[name].' + version + '.js'
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
