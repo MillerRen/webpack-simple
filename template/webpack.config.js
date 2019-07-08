cefd041ffb423d7ddfca5211085f5700109afaa6
MillerRen <ren12345@126.com>
2019-07-08 10:45:11 +0800

    syntax

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         use: [
           'vue-style-loader',
           'css-loader'
-        ],
+        ]
       },{{#sass}}
       {
         test: /\.scss$/,
@@ -25,7 +25,7 @@ module.exports = {
           'vue-style-loader',
           'css-loader',
           'sass-loader'
-        ],
+        ]
       },
       {
         test: /\.sass$/,
